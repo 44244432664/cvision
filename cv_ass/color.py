aaba66fa90452314318d671ebf7ffa8450d65e5a
44244432664 <113764159+44244432664@users.noreply.github.com>
2023-06-05 21:30:06 +0700

    Add files via upload

--- cv_ass/color.py
+++ cv_ass/color.py
@@ -8,32 +8,34 @@ grayimg = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 print(grayimg.shape)
 print(grayimg.dtype)
 
-# cv2.imshow('gray_img', grayimg)
-# cv2.waitKey()
-# cv2.destroyAllWindows()
-
-# gray = grayimg.reshape(-1)
-# print(gray.shape)
-# cv2.imshow('gray_img', gray)
-# cv2.waitKey()
-# cv2.destroyAllWindows()
-
 mat1 = np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]])
 mat2 = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
-avg = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]])
+dil = np.array([    [0, 1, 0],
+                    [1, 1, 1],
+                    [0, 1, 0]])
+
+dil2 = np.array([   [0, 0, 1, 0, 0],
+                    [0, 1, 1, 1, 0],
+                    [1, 1, 1, 1, 1],
+                    [0, 1, 1, 1, 0],
+                    [0, 0, 1, 0, 0]])
+
+dil = np.uint8(dil)
+dil2 = np.uint8(dil2)
+kernel = np.ones((3,3), np.uint8)
 # mat1 = np.uint8(mat1)
 print(mat1.dtype)
 
 
 
 res1 = cv2.filter2D(src=grayimg, kernel=mat1, ddepth=-1)
-cv2.imwrite('vert.jpg', res1)
-cv2.imshow('mask1', res1)
+cv2.imwrite('vert_dect.jpg', res1)
+# cv2.imshow('vert_dect', res1)
 cv2.waitKey()
 
 res2 = cv2.filter2D(src=grayimg, kernel=mat2, ddepth=-1)
-cv2.imwrite('hor.jpg', res2)
-cv2.imshow('mask2', res2)
+cv2.imwrite('hor_dect.jpg', res2)
+# cv2.imshow('hor_dect', res2)
 cv2.waitKey()
 
 
@@ -41,7 +43,33 @@ res = (res1 + res2)
 # res = cv2.filter2D(src=res, kernel=avg, ddepth=-1)
 # res //= 9
 cv2.imwrite('res.jpg', res)
-cv2.imshow('mask', res)
+# cv2.imshow('mask', res)
 cv2.waitKey()
 
-cv2.destroyAllWindows()
+img_erosion = cv2.erode(res, kernel, iterations=1)
+img_dilation = cv2.dilate(res, kernel, iterations=1)
+
+try_e = cv2.erode(res, dil, iterations=1)
+try_d = cv2.dilate(res, dil, iterations=1)
+
+opening = cv2.dilate(cv2.erode(res, kernel, iterations=1), kernel, iterations=1)
+closing = cv2.erode(cv2.dilate(res, kernel, iterations=1), kernel, iterations=1)
+
+try_o = cv2.dilate(cv2.erode(res, dil, iterations=1), dil, iterations=1)
+try_c = cv2.erode(cv2.dilate(res, dil, iterations=1), dil, iterations=1)
+
+cv2.imwrite('erode.jpg', img_erosion)
+cv2.imwrite('dilate.jpg', img_dilation)
+cv2.imwrite('try_e.jpg', try_e)
+cv2.imwrite('try_d.jpg', try_d)
+cv2.imwrite('opening.jpg', opening)
+cv2.imwrite('closing.jpg', closing)
+cv2.imwrite('try_o.jpg', try_o)
+cv2.imwrite('try_c.jpg', try_c)
+
+# cv2.imshow('Erosion', img_erosion)
+# cv2.imshow('Dilation', img_dilation)
+# cv2.imshow('Dilation', img_dilation2)
+# cv2.waitKey(0)
+
+# cv2.destroyAllWindows()
